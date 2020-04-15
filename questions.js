f8a5329d165f00c4365e67180347a10ad476b213
m1omg <62194279+m1omg@users.noreply.github.com>
2020-04-16 00:57:02 +0200

    Update questions.js

--- questions.js
+++ questions.js
@@ -24,7 +24,7 @@ questions =
             "Repression of dissent is justifiable",
             "A significant degree of coercion is a necessary evil for a society.",
             "Only the government can fairly and effectively regulate organisations.",
-            "Drug posession should be a criminal offense.",
+            "Society requires a strong bureaucracy in order to function.",
             "The death penalty should exist for certain crimes",
             "In times of crisis, safety becomes more important than civil liberties.",
             "If you have nothing to hide, you have nothing to fear."
@@ -32,15 +32,15 @@ questions =
         [//Liberty
             "The government should be less involved in the day to day life of its citizens.",
             "A democratic and/or libertarian society is generally preferable to an authoritarian one.",
-            "Educating people so they can make better decisions for themselves is preferable to enforcing them by the state",
+            "Educating people so they can make better decisions for themselves is preferable to enforcing them by strict laws",
             "The state should be abolished",
             "Most services currently provided by the government should be provided by the community or the free market",
-            "An individual’s body is their own property, and they should be able to do anything they desire to it and the same applies to their interactions with another consenting person.",
+            "The police as it currently exists was not made to protect the people, but to uphold the status-quo by force.",
             "State schools and state owned media are a bad idea because the state can use them to push its own agenda"
         ]],[//Progressive-Conservative
         [//Progressive
-            "The value of one's opinions on a given issue depends on their priviledge in society",
-            "Members of disadvantaged groups that have previously suffered discrimination should be given advantages over the rest of the population to redress past wrongs done to them",
+            "Two individuals above the age of consent should be able to do whatever they want with each other if they agree to it, even if it makes me uncomfortable.",
+            "An individual’s body is their own property, and they should be able to do anything they desire to it.",
             "A person should be able to worship whomever or whatever they want.",
             "Nudism is perfectly natural.",
             "Animals deserve certain universal rights.",
@@ -51,7 +51,7 @@ questions =
         [//Conservative
             "Only heterosexual marriage should be recognised.",
             "LGBT individuals should not be able to adopt children.",
-            "People should always follow traditional gender roles",
+            "Drug posession should be a criminal offense.",
             "Traditional values and culture should be preserved.",
             "Victimless crimes should still be punished.",
             "One cannot be moral without being religious.",
